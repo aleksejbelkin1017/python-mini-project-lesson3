cb8bf1489ab63ce2364a95e7f87af31d64c2f01a
Aleksej Belkin <aleksejbelkin1017@yandex.ru>
2024-12-15 15:24:59 +0300

    init

---
+++ src/masks.py
@@ -0,0 +1,28 @@
+from typing import Union
+
+
+def get_mask_card_number(card_number: Union[int, str]) -> Union[str]:
+    """Принимает на вход номер карты и возвращает её маску.
+    Видны первые 6 цифр и последние 4 цифры. Остальные символы отображаются *.
+    Номер разбит по блокам по 4 цифры."""
+    # преобразуем принятый номер карты к строке
+    card_number = str(card_number)
+    # наносим маску на номер карты
+    mask_number = card_number.replace(card_number[6:-4], "*" * len(card_number[6:-4]))
+    # разбиваем замаскированный номер на блоки по 4 символа
+    card_blocks = mask_number[0:4] + " " + mask_number[4:8] + " " + mask_number[8:12] + " " + mask_number[12:]
+    return card_blocks
+
+
+def get_mask_account(account_number: Union[int, str]) -> Union[str]:
+    """Принимает на вход номер счёта и возвращает его маску.
+    Номер счёта возвращается в виде ** и 4 последние цифры."""
+    # преобразуем принятый номер счёта к строке
+    account_number = str(account_number)
+    # наносим маску на номер счёта
+    mask_number = "**" + account_number[-4:]
+    return mask_number
+
+
+print(get_mask_card_number(1234567890123456))
+print(get_mask_account(1234567890123456))
