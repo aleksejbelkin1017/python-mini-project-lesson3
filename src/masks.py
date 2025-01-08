9eccbe9a7cc5ad17bea58a7e6655c5d250b303d8
Aleksej Belkin <aleksejbelkin1017@yandex.ru>
2025-01-08 20:59:09 +0300

    add tests in module test_masks.py and create clear module conftest.py

--- src/masks.py
+++ src/masks.py
@@ -5,13 +5,20 @@ def get_mask_card_number(card_number: Union[int, str]) -> Union[str]:
     """Принимает на вход номер карты и возвращает её маску.
     Видны первые 6 цифр и последние 4 цифры. Остальные символы отображаются *.
     Номер разбит по блокам по 4 цифры."""
-    # преобразуем принятый номер карты к строке
+    # # преобразуем принятый номер карты к строке
     card_number = str(card_number)
-    # наносим маску на номер карты
-    mask_number = card_number.replace(card_number[6:-4], "*" * len(card_number[6:-4]))
-    # разбиваем замаскированный номер на блоки по 4 символа
-    card_blocks = mask_number[0:4] + " " + mask_number[4:8] + " " + mask_number[8:12] + " " + mask_number[12:]
-    return card_blocks
+    # # наносим маску на номер карты
+    # mask_number = card_number.replace(card_number[6:-4], "*" * len(card_number[6:-4]))
+    # # разбиваем замаскированный номер на блоки по 4 символа
+    # card_blocks = mask_number[:4] + " " + mask_number[4:8] + " " + mask_number[8:12] + " " + mask_number[12:]
+    # return card_blocks
+    if card_number == "":
+        raise IndexError("Отсутствует обязательный аргумент при вводе номера карты")
+    elif len(card_number) != 16:
+        raise IndexError("Длина номера карты должна быть 16 цифр")
+    elif not card_number.isdigit():
+        raise ValueError("Номер карты должен состоять только из цифр")
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
 def get_mask_account(account_number: Union[int, str]) -> Union[str]:
@@ -20,9 +27,14 @@ def get_mask_account(account_number: Union[int, str]) -> Union[str]:
     # преобразуем принятый номер счёта к строке
     account_number = str(account_number)
     # наносим маску на номер счёта
-    mask_number = "**" + account_number[-4:]
-    return mask_number
+    if account_number == "":
+        raise IndexError("Отсутствует обязательный аргумент при вводе номера счёта")
+    elif len(account_number) != 20:
+        raise IndexError("Длина номера счёта должна быть 20 символов")
+    else:
+        mask_number = "**" + account_number[-4:]
+        return mask_number
 
 
-# print(get_mask_card_number(1234567890123456))
-# print(get_mask_account(1234567890123456))
+# print(get_mask_card_number(""))
+print(get_mask_account(12345678901234567891))
---
+++ tests/test_masks.py
@@ -0,0 +1,63 @@
+# Модуль masks
+
+# get_mask_card_number:
+import pytest
+from src.masks import get_mask_card_number, get_mask_account
+
+@pytest.mark.parametrize(
+    "card_number,expected_result", [
+        (4111111111111111, "4111 11** **** 1111"),
+        (3782822463100050, "3782 82** **** 0050"),
+        (5555555555555550, "5555 55** **** 5550"),
+        (5621235686512354, "5621 23** **** 2354")
+    ]
+)
+def test_card_number_positive_cases(card_number, expected_result):
+    result = get_mask_card_number(card_number)
+    assert result == expected_result
+
+
+def test_card_number_wrong_len():
+    with pytest.raises(IndexError) as exc_info:
+        get_mask_card_number("1234567890123")
+    assert str(exc_info.value) == "Длина номера карты должна быть 16 цифр"
+
+
+def test_card_number_is_not_digit():
+    with pytest.raises(ValueError) as exc_info:
+        get_mask_card_number("abcd1234efgh5678")
+    assert str(exc_info.value) == "Номер карты должен состоять только из цифр"
+
+
+def test_card_number_is_clear():
+    with pytest.raises(IndexError) as exc_info:
+        get_mask_card_number("")
+    assert str(exc_info.value) == "Отсутствует обязательный аргумент при вводе номера карты"
+
+
+# get_mask_account:
+@pytest.mark.parametrize(
+    "account_number, expected_account_number", [
+        (12345678901234567891, "**7891"),
+        (37556482822463100050, "**0050"),
+        ("abcd12dds34eafgh5678", "**5678"),
+        ("SDcs568844sddac53256", "**3256"),
+    ]
+)
+
+
+def test_get_mask_account_positive(account_number, expected_account_number):
+    result = get_mask_account(account_number)
+    assert result == expected_account_number
+
+
+def test_get_mask_account_wrong_len_account_number():
+    with pytest.raises(IndexError) as exc_info:
+        get_mask_account("1234567890123")
+    assert str(exc_info.value) == "Длина номера счёта должна быть 20 символов"
+
+
+def test_get_mask_account_number_is_clear():
+    with pytest.raises(IndexError) as exc_info:
+        get_mask_account("")
+    assert str(exc_info.value) == "Отсутствует обязательный аргумент при вводе номера счёта"
